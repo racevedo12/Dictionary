e034fb62fc6ac89b040cc2a75ed8551f3f603607
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-10 17:36:30 -0400

    Changed the WordList component a bit to make it look more organized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import ShowWordDetails from "./Components/ShowWordDetails/ShowWordDetails";
 
 function App() {
 
   return (
 
     <div>
-      <Route path="/" component={Home}/>
+      <Route path="/" exact component={Home}/>
+      <Route path="/word/:theWord" exact component={ShowWordDetails} />
     </div>
 
   );
