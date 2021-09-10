9a6ccf63687bad93ed52678718e4855806a1643d
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-10 14:01:16 -0400

    Testing api

--- src/Components/Word/Word.js
+++ src/Components/Word/Word.js
@@ -1,13 +1,28 @@
 import React, {useState, useEffect} from "react";
 
-const Word = ( {word} ) =>
+const Word = ( ) =>
 {
     const [data, setData] = useState({});
 
     const getData = () =>
     {
-        const url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
+        const url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+        const axios = require('axios').default;
+
+        axios.get(url)
+        .then( res =>
+        {
+            setData(res.data);
+        })
     };
+
+    useEffect( () =>
+    {
+        getData();
+        // eslint-disable-next-line
+    }, [])
+
+    console.log(data);
 
     return (
         <h3>Word</h3>
