1070f74054cee1df2ea2c4eeff26863e805b1a3f
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-10 12:46:45 -0400

    Added basic design for the form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./form.css";
 
 const Form = () =>
 {
     return (
-        <h3>Yep</h3>
+
+        <form className="form">
+            <input id="word" placeholder="Type in the word that you want to look for"></input>
+            <button id="search-button">Search</button>
+        </form>
     );
 };
 
