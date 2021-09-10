4e79165d8285ec13f85e868f59e20562703097b0
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-10 13:58:17 -0400

    WordList component created

---
+++ src/Components/WordList/WordList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Word from "../Word/Word";
+
+const WordList = () =>
+{
+    return (
+        <div>
+            <Word />
+        </div>
+    );
+};
+
+export default WordList;
