c3f5dc546f8a958a4f5b81e392ba12ea2695f0a1
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-11 13:29:53 -0400

    Added useState and useEffect to get the data from the api and save it into the state to use it later in the jsx

--- src/Components/ShowWordDetails/ShowWordDetails.js
+++ src/Components/ShowWordDetails/ShowWordDetails.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./show-word-details.css";
 
-const ShowWordDetails = () =>
+const ShowWordDetails = ( {match} ) =>
 {
 
+    const [data, setData] = useState({});
+
+    const getData = () =>
+    {
+        const word = match.params.theWord;
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}` ;
+        const axios = require('axios').default;
+
+        axios.get(url)
+        .then( res =>
+        {
+            setData(res.data[0]);
+        });
+
+    };
+
+    useEffect( () =>
+    {
+        getData();
+        // eslint-disable-next-line
+    }, []);
+
+    
     return (
 
         <div className="show-details-container">
@@ -14,6 +37,7 @@ const ShowWordDetails = () =>
             <div className="show-word-header">
                 <h1>Dictionary App</h1>
             </div>
+            
             
         </div>
         
