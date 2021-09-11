bacdbb4833d887413c96009cbcf32eaec94ce47b
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-11 12:59:48 -0400

    Added comments into the WordList component

--- src/Components/WordList/WordList.js
+++ src/Components/WordList/WordList.js
@@ -9,21 +9,38 @@ const WordList = ( {list, invalidWord, setInvalidWord} ) =>
         <div>
 
             <h2> Word List </h2>
+
+            {/*
+                Loop through the array of words
+                Creates a word component for each word
+                And creates a link for each component specifically 
+             */}
             {list.map( (word, idx) =>
             {
                 return (
 
                     <div key={idx}>
+
                         <h3>
-                            <Link to={`/${word}`}> 
-                                <Word word={word} invalidWords={invalidWord} setInvalidWord={setInvalidWord}/> 
+
+                            <Link to={`/${word}`}>
+
+                                <Word 
+                                    word={word} 
+                                    invalidWords={invalidWord} 
+                                    setInvalidWord={setInvalidWord}
+                                /> 
+
                             </Link>
+
                         </h3>
+
                     </div>
                     
                 );
                 
             })}
+
         </div>
     );
 };
